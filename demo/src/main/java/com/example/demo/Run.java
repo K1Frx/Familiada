6fbd1bad62ea7cd4d7145e479601e911b61fadaa
Kamil <k.fryszkowski.02@gmail.com>
2022-06-18 18:33:00 +0200

    Class Question.java created Operator panel created: showing answers and questions, clickable buttons to show answers, added some test data

--- demo/src/main/java/com/example/demo/Run.java
+++ demo/src/main/java/com/example/demo/Run.java
@@ -7,14 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Run extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Run.class.getResource("back.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {
